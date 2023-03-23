aadd5e311225459ce9cbf01c53f32197ab9a5107
karanjc <66589659+karanjc@users.noreply.github.com>
2023-03-23 22:54:22 +0530

    Update news.js

--- src/news.js
+++ src/news.js
@@ -32,7 +32,7 @@ class News extends React.Component {
               <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#coding</span>
               <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#Akhilesh</span>
               <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#AI</span>
-            </div>
+            </div><br/>
           </div>
         </div>
         );
